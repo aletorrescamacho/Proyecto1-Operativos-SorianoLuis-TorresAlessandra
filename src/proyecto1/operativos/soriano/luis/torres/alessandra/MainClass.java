e108713f1e029ade20971774ebeb3a6adf7568d5
aletorrescamacho <alessandra.torres@correo.unimet.edu.ve>
2025-01-23 17:47:47 -0400

    pestañasinterfaz

--- src/proyecto1/operativos/soriano/luis/torres/alessandra/MainClass.java
+++ src/proyecto1/operativos/soriano/luis/torres/alessandra/MainClass.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyecto1.operativos.soriano.luis.torres.alessandra;
-
+import ui.MainWindow;
 /**
  *
  * @author Aless
@@ -15,6 +15,10 @@ public class MainClass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        MainWindow mainWindow = new MainWindow(); // Reemplaza "MainWindow" con el nombre de tu clase JFrame
+        mainWindow.setVisible(true);
     }
     
 }
