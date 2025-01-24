605388e24dc0078e5b3c59a285b901fb472e8861
aletorrescamacho <alessandra.torres@correo.unimet.edu.ve>
2025-01-24 12:07:21 -0400

    MAINCLASS

--- src/proyecto1/operativos/soriano/luis/torres/alessandra/MainClass.java
+++ src/proyecto1/operativos/soriano/luis/torres/alessandra/MainClass.java
@@ -3,13 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyecto1.operativos.soriano.luis.torres.alessandra;
+import entities.CPU;
+import entities.Manejotxt;
 import entities.Proceso;
-import ui.MainWindow; //
+import ui.MainWindow; //3
 /**
  *
  * @author Aless
- */
+ *////
 public class MainClass {
+    public static CPU cpu1 = new CPU(); // CPU 1 siempre activo
+    public static CPU cpu2 = new CPU(); // CPU 2 siempre activo
+    public static CPU cpu3 = new CPU(); // CPU 3 dependerá de CPUsActivos
+        
 
     /**
      * @param args the command line arguments
@@ -27,6 +33,23 @@ public class MainClass {
         // Crear un proceso I/O bound
         Proceso proceso2 = new Proceso("Proceso2", 200, "I/O bound", 5, 10);
         System.out.println(proceso2);
+        
+        
+        
+        
+
+        int cpusActivos = Manejotxt.leerCPUsActivos();
+        if (cpusActivos == 3) {
+            cpu3.setActivo(true); // CPU 3 activo si CPUsActivos es 3
+        } else {
+            cpu3.setActivo(false); // CPU 3 desactivado si CPUsActivos es menor a 3
+        }
+
+
+        // Imprimir el estado inicial de las CPUs
+        System.out.println(cpu1); // CPU{id=1, proceso=Sin proceso asignado, activo=true}
+        System.out.println(cpu2); // CPU{id=2, proceso=Sin proceso asignado, activo=true}
+        System.out.println(cpu3); // CPU{id=3, proceso=Sin proceso asignado, activo=true}
     }
     
 }
